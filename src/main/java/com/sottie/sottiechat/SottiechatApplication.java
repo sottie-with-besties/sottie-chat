32482eb778d9ce9cb0a3aea1347185934ab5da1f
SeungGun <saverrw2864@naver.com>
2024-04-09 15:45:21 +0900

    Initialize Project

---
+++ src/main/java/com/sottie/sottiechat/SottiechatApplication.java
@@ -0,0 +1,13 @@
+package com.sottie.sottiechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SottiechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SottiechatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sottie/sottiechat/SottiechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sottie.sottiechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SottiechatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
