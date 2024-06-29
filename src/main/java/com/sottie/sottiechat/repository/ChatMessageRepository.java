38f0fe454c5eeb6b45aaaffedfd1dac85a42706a
SeungGun <saverrw2864@naver.com>
2024-06-29 20:45:29 +0900

    feat: 입장 채팅 데이터 조회 쿼리문 작성

--- src/main/java/com/sottie/sottiechat/repository/ChatMessageRepository.java
+++ src/main/java/com/sottie/sottiechat/repository/ChatMessageRepository.java
@@ -2,8 +2,14 @@ package com.sottie.sottiechat.repository;
 
 import com.sottie.sottiechat.domain.ChatMessage;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    @Query("{'chatRoomId' : ?0, 'senderId' : ?1, 'chatType' : 'ENTRANCE'}")
+    List<ChatMessage> findByChatRoomIdAndSenderIdWithEntrance(Long chatRoomId, Long senderId);
 }
