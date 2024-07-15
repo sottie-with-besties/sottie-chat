db92922e0541370fdacbe8af67277f4088cf50fc
SeungGun <saverrw2864@naver.com>
2024-07-15 09:29:38 +0900

    feat: 팩토리 메소드 구현

--- src/main/java/com/sottie/sottiechat/dto/MessageResponse.java
+++ src/main/java/com/sottie/sottiechat/dto/MessageResponse.java
@@ -8,6 +8,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 public class MessageResponse {
 
     @Data
@@ -21,5 +23,27 @@ public class MessageResponse {
         private MessageType messageType;
         private ChatType chatType;
         private Status status;
+
+        public static MessageResponse.Chat from(MessageRequest.Enter message) {
+            return MessageResponse.Chat.builder()
+                    .sender(message.getSender())
+                    .contents(message.getSender().getNickname() + "님이 채팅방에 참여했습니다.")
+                    .messageType(MessageType.TEXT)
+                    .chatType(ChatType.ENTRANCE)
+                    .status(Status.SUCCESS)
+                    .timestamp(LocalDateTime.now().toString())
+                    .build();
+        }
+
+        public static MessageResponse.Chat from(MessageRequest.Chat message) {
+            return Chat.builder()
+                    .sender(message.getSender())
+                    .contents(message.getContents())
+                    .messageType(MessageType.TEXT)
+                    .chatType(ChatType.CHAT)
+                    .status(Status.SUCCESS)
+                    .timestamp(LocalDateTime.now().toString())
+                    .build();
+        }
     }
 }
