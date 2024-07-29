123414c4f5cbc1198fd6d7795e824ac7013d646c
SeungGun <saverrw2864@naver.com>
2024-07-29 10:47:15 +0900

    feat: 채팅 메시지 페이징으로 조회 쿼리 aggregation & pipeline으로 구현

--- src/main/java/com/sottie/sottiechat/repository/ChatMessageRepository.java
+++ src/main/java/com/sottie/sottiechat/repository/ChatMessageRepository.java
@@ -1,6 +1,7 @@
 package com.sottie.sottiechat.repository;
 
 import com.sottie.sottiechat.domain.ChatMessage;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,4 +13,22 @@ public interface ChatMessageRepository extends MongoRepository<ChatMessage, Stri
 
     @Query("{'chatRoomId' : ?0, 'senderId' : ?1, 'chatType' : 'ENTRANCE'}")
     List<ChatMessage> findByChatRoomIdAndSenderIdWithEntrance(Long chatRoomId, Long senderId);
+
+    @Aggregation(
+            pipeline = {
+                    "{ $match : { 'chatRoomId' : ?0 } }",
+                    "{ $sort : { '_id' : -1 } }",
+                    "{ $limit : ?1 }"
+            }
+    )
+    List<ChatMessage> findLatestChat(Long chatRoomId, int limit);
+
+    @Aggregation(
+            pipeline = {
+                    "{ $match : { '_id' : { '$lt' : {'$oid' : ?1 } }, 'chatRoomId' : ?0 } }", // _id 비교는 ObjectId() 비교를 위해 $oid 사용
+                    "{ $sort : { '_id' : -1 } }",
+                    "{ $limit : ?2 }"
+            }
+    )
+    List<ChatMessage> findChatByPaging(Long chatRoomId, String lastId, int limit);
 }
