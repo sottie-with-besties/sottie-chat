d3a3dc7d1e58a02bb2abf710ffd40c87ed1a9267
SeungGun <saverrw2864@naver.com>
2025-03-22 16:04:36 +0900

    chore: RabbitMQ property 호스트 이름 오류 수정

--- src/main/java/com/sottie/sottiechat/config/RabbitConfigProperties.java
+++ src/main/java/com/sottie/sottiechat/config/RabbitConfigProperties.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties("spring.rabbitmq")
 public class RabbitConfigProperties {
-    private String hostName;
+    private String host;
     private int port;
     private String username;
     private String password;
