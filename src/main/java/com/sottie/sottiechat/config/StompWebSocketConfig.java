b491b5139c34ca45658800f92eb79d7f7c38135e
SeungGun <saverrw2864@naver.com>
2025-03-22 16:05:31 +0900

    chore: 외부 브로커 연결을 위한 Relay 설정 추가

--- src/main/java/com/sottie/sottiechat/config/StompWebSocketConfig.java
+++ src/main/java/com/sottie/sottiechat/config/StompWebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.sottie.sottiechat.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -13,8 +14,11 @@ import org.springframework.web.socket.config.annotation.WebSocketTransportRegist
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
+@EnableConfigurationProperties(RabbitConfigProperties.class)
 public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
     private final WebSocketInterceptor interceptor;
+    private final RabbitConfigProperties properties;
+    private static final int STOMP_PORT = 61613;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -27,6 +31,13 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setPathMatcher(new AntPathMatcher("."));
         registry.setApplicationDestinationPrefixes("/pub");
         registry.enableStompBrokerRelay("/queue", "/topic", "/exchange", "/amq/queue")
+                .setRelayHost(properties.getHost())
+                .setRelayPort(STOMP_PORT)
+                .setSystemLogin(properties.getUsername())
+                .setSystemPasscode(properties.getPassword())
+                .setClientLogin(properties.getUsername())
+                .setClientPasscode(properties.getPassword())
+                .setVirtualHost("/")
                 .setSystemHeartbeatSendInterval(30000) // Heartbeat 전송 주기
                 .setSystemHeartbeatReceiveInterval(30000); // Heartbeat 수신 주기
     }
