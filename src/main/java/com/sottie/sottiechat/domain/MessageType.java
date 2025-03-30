def006c4b0ee40aca3b68a3f11c2a4683dc701e3
SeungGun <saverrw2864@naver.com>
2025-03-30 20:24:29 +0900

    feat: Enum 타입 Json String 포맷화

--- src/main/java/com/sottie/sottiechat/domain/MessageType.java
+++ src/main/java/com/sottie/sottiechat/domain/MessageType.java
@@ -1,5 +1,8 @@
 package com.sottie.sottiechat.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum MessageType {
     TEXT,
     IMAGE,
