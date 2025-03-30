9205397b06a2cf2fde45fd56cfb7693529c8c032
SeungGun <saverrw2864@naver.com>
2025-03-30 20:26:54 +0900

    refactor: 채팅 목록 조회 개선

--- src/main/java/com/sottie/sottiechat/service/MessageQueryService.java
+++ src/main/java/com/sottie/sottiechat/service/MessageQueryService.java
@@ -2,7 +2,7 @@ package com.sottie.sottiechat.service;
 
 import com.sottie.sottiechat.domain.ChatMessage;
 import com.sottie.sottiechat.domain.LastReadStatus;
-import com.sottie.sottiechat.dto.MessageResponse;
+import com.sottie.sottiechat.dto.ChatMessageDateGroup;
 import com.sottie.sottiechat.repository.ChatMessageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,32 +20,26 @@ public class MessageQueryService {
     private final ChatMessageRepository chatMessageRepository;
     private final CryptoService cryptoService;
     private final MongoTemplate mongoTemplate;
+    private static final int CURRENT_PAGING_SIZE = 30;
 
     public ChatMessage getMessage(String messageId) {
         return chatMessageRepository.findById(messageId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 메시지입니다."));
     }
 
-    public List<MessageResponse.Chat> getChatMessagesByPaging(Long roomId, String cursor, int size) {
-        if (size < 1)
-            throw new IllegalArgumentException("페이지 개수가 유효하지 않습니다.");
+    public List<ChatMessageDateGroup> getChatMessagesByPaging(Long userId, Long roomId, String cursor, boolean useDecode) {
+        if (cursor == null) // 최신 채팅 조회
+            return buildMessages(chatMessageRepository.findLatestChatByPagingGroupByDate(userId, roomId, CURRENT_PAGING_SIZE), useDecode);
 
-        // TODO: 일별로 response 정렬하기
-        if (cursor == null) // 최초 페이지
-            return buildMessages(chatMessageRepository.findLatestChat(roomId, size));
-
-        return buildMessages(chatMessageRepository.findChatByPaging(roomId, cursor, size));
+        return buildMessages(chatMessageRepository.findChatByPagingGroupByDate(userId, roomId, cursor, CURRENT_PAGING_SIZE), useDecode);
     }
 
-    private List<MessageResponse.Chat> buildMessages(List<ChatMessage> chatMessages) {
-        return chatMessages.stream().map((c) -> MessageResponse.Chat.builder()
-                .messageId(c.getId())
-                .contents(cryptoService.decodeAES(c.getContents()))
-                .userId(c.getUserId())
-                .timestamp(c.getTimestamp().toString())
-                .messageType(c.getMessageType())
-                .chatType(c.getChatType())
-                .status(c.getStatus()).build()).toList();
+    private List<ChatMessageDateGroup> buildMessages(List<ChatMessageDateGroup> chatMessages, boolean useDecode) {
+        chatMessages.stream()
+                .flatMap(dateGroup -> dateGroup.getChats().stream())
+                .filter(chatMessage -> useDecode)
+                .forEach(chatMessage -> chatMessage.updateContents(cryptoService.decodeAES(chatMessage.getContents())));
+        return chatMessages;
     }
 
     public List<LastReadStatus> getReadStatusByChatRoom(Long roomId) {
