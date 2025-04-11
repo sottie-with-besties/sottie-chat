69031ffda777718e0cf41c46d9ebf68195b57255
SeungGun <saverrw2864@naver.com>
2025-04-11 20:26:09 +0900

    refactor: 이름 변경에 따른 수정

--- src/main/java/com/sottie/sottiechat/dto/MessageRequest.java
+++ src/main/java/com/sottie/sottiechat/dto/MessageRequest.java
@@ -1,6 +1,6 @@
 package com.sottie.sottiechat.dto;
 
-import com.sottie.sottiechat.domain.ChatType;
+import com.sottie.sottiechat.domain.EventType;
 import com.sottie.sottiechat.domain.MessageType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,19 +13,11 @@ public class MessageRequest {
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
-    public static class Enter {
-        private Long userId;
-    }
-
-    @Data
-    @AllArgsConstructor
-    @NoArgsConstructor
-    @Builder
     public static class Chat {
         private Long userId;
         private String contents;
         private MessageType messageType;
-        private ChatType chatType;
+        private EventType eventType;
     }
 
     @Data
